da083bd3f3e418391252a6b7281e047fd0807117
Rachel Spencer <rachelspencer688@gmail.com>
2022-08-22 22:18:38 -0400

    Final

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -1,5 +1,12 @@
-let x = 1
-let y = 2
+let x = 1;
+let y = 2;
 
 // Write your code below this line.
 
+if (x > y){
+    console.log(`${x} is larger than ${y}.`);
+} else if (y > x){
+    console.log(`${y} is larger than ${x}.`);
+} else {
+    console.log("The values are identical.");
+}
